3e5c8164e8a841d8ab902a5a1edddabbefb558fb
Gianluca Perrone <perronegian@hotmail.com>
2019-09-10 04:55:43 -0300

    infinite scrolling with pagy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import {infScrollInit} from "components/infinite_scroll.js"
+
+
+
+infScrollInit();
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+250.times do
+
+	Post.create({
+		title: Faker::GreekPhilosophers.name,
+		description: Faker::GreekPhilosophers.quote
+	})
+
+end
